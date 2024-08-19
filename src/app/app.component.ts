0d038de816044e69ff24acd851499ad0cae9ad1f
Prasthuth Pradeep <prasthuth@gmail.com>
2024-08-19 16:07:19 +0530

    Included button lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/buttons/buttons.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButton, MatAnchor, MatFabButton, MatFabAnchor, MatMiniFabButton, MatMiniFabAnchor, MatIconButton, MatIconAnchor } from '@angular/material/button';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatIcon} from '@angular/material/icon';
+import {MatTooltip} from '@angular/material/tooltip';
+
+@Component({
+  selector: 'app-buttons',
+  standalone: true,
+  imports: [
+    MatButton,
+    MatAnchor,
+    MatFabButton,
+    MatFabAnchor,
+    MatMiniFabButton,
+    MatMiniFabAnchor,
+    MatIconButton,
+    MatIconAnchor,
+    MatIcon,
+    MatTooltip,
+    MatCheckbox,
+    FormsModule,
+  ],
+  templateUrl: './buttons.component.html',
+  styleUrl: './buttons.component.scss'
+})
+export class ButtonsComponent {
+  isDisabled = false;
+  clickCounter = 0;
+  toggleDisable = false;
+  tooltipText = 'This is a button tooltip!';
+  disabledInteractive = false;
+}
